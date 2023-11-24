bb565e699879c08b8de2eca402ef6fd61137ac31
Castro <igordc38@gmail.com>
2023-11-23 21:03:22 -0300

    ICA [feature] main colors for global control

---
+++ src/global/colors.ts
@@ -0,0 +1,15 @@
+export const Colors = {
+  purple: {
+    lighter: "#EEF0F5",
+    medium: "#EECFFC",
+    bolder: "#7B1FA2",
+  },
+  white: "#FFFFFF",
+  green: "#02A117",
+  blue: "#00A296",
+  gray: {
+    lighter: "#6D6F73",
+    bolder: "#393A3C",
+  },
+  black: "#202326",
+};
